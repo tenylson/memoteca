3e2d0ebb97c9238f7bbc66156608dfe4d3ecc088
Luciano Tenylson <tenylson@tjro.jus.br>
2024-06-07 09:49:11 -0400

    04.02 - Listando com *ngFor

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -7,6 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListarPensamentoComponent implements OnInit {
 
+  listaPensamentos = [
+    {
+      conteudo: 'Comunicação entre componentes',
+      autoria: 'Angular',
+      modelo: 'modelo3'
+    }
+
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
