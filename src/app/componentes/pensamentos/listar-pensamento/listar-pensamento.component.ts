0d3dce38f0d25fd1041240d84d510fe6f5a1677c
Luciano Tenylson <tenylson@tjro.jus.br>
2024-06-07 11:35:52 -0400

    05.04 - Implementando interfaces

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Pensamento } from '../pensamento';
 
 @Component({
   selector: 'app-listar-pensamento',
@@ -7,24 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListarPensamentoComponent implements OnInit {
 
-  listaPensamentos = [
-    {
-      conteudo: 'Passando parametros entre componentes filho',
-      autoria: 'Componente pai',
-      modelo: 'modelo1'
-    },
-    {
-      conteudo: 'Minha propiedade é decorada com @Input() ',
-      autoria: 'Componente filho',
-      modelo: 'modelo2'
-    },
-    {
-      conteudo: 'Mussum Ipsum, cacilds vidis litro abertis.  Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis. Hoje é sexta-feris, dia da maldadis! Eu nunca mais boto a boca num copo de cachaça, agora eu só uso canudis! Praesent vel viverra nisi. Mauris aliquet nunc non turpis scelerisque, eget.',
-      autoria: 'Mussum Ipsum',
-      modelo: 'modelo3'
-    }
-
-  ]
+  listaPensamentos: Pensamento[] = []
 
   constructor() { }
 
